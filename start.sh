1f1db8239d79601c0439de204da5f9a6646677b7
Abah Rafka <rafkasaurus@gmail.com>
2026-02-01 19:58:42 +0700

    ganti lokal dev aja dlu

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run database migrations
-echo "Running database migrations..."
-npx drizzle-kit migrate --config=drizzle.config.ts
-
-# Start the Nuxt server
-echo "Starting Nuxt server..."
-node .output/server/index.mjs
